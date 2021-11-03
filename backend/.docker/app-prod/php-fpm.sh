d8fa1271b545845995a5b7622ece082d25989615
Aitor Errasti <azarparalelo@gmail.com>
2021-11-03 10:25:43 +0100

    Add new logic to reset database on build dockerfile

--- backend/.docker/app-prod/php-fpm.sh
+++ backend/.docker/app-prod/php-fpm.sh
@@ -8,6 +8,12 @@
 # file that was distributed with this source code.
 
 console cache:warmup --no-interaction
+
+if [ "${APP_RESET_ON_BUILD:-}" = true ]; then
+    console doctrine:database:drop --no-interaction --force
+    console doctrine:database:create --no-interaction
+fi
+
 console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 php-fpm --allow-to-run-as-root
