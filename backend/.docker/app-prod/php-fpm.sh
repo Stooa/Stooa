dff11bc4957b7194e8b35bc743d2d3a4895526b3
Jordi <jordism91@gmail.com>
2021-11-04 14:01:01 +0100

    Dump autoload when installing dev dependencies

--- backend/.docker/app-prod/php-fpm.sh
+++ backend/.docker/app-prod/php-fpm.sh
@@ -10,14 +10,17 @@
 # Can be used on staging environments to destroy the database each time you deploy
 # the application, to ensure you start with the initial data each time
 if [ "${RESET_DATABASE:-}" = true ]; then
+    echo 'Resetting database...'
+
     # Install development dependencies to be able to use doctrine/doctrine-fixtures-bundle
     # console commands to load fixtures after database creation
     composer install --prefer-dist --no-progress --no-interaction
+    composer dump-autoload --classmap-authoritative
 
     console doctrine:database:drop --no-interaction --force
     console doctrine:database:create --no-interaction
     console doctrine:schema:update --no-interaction --force
-    console doctrine:fixtures:load --no-interaction
+    console doctrine:fixtures:load --no-interaction --append
 
     # Our current infrastructure does not allow to run sidecar containers to perform
     # this operation on a separate container, so we have to run it before launching php-fpm
@@ -25,11 +28,14 @@ if [ "${RESET_DATABASE:-}" = true ]; then
 
     # Install production dependencies (due to our limitations in infrastructure)
     composer install --prefer-dist --no-progress --no-dev --no-interaction
+    composer dump-autoload --classmap-authoritative
 fi
 
 # Can be used on production environments to apply migrations to the database each time
 # you deploy the application, to ensure you start with the database in a correct state
 if [ "${MIGRATE_DATABASE:-}" = true ]; then
+    echo 'Applying migrations to database...'
+
     console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
     # Our current infrastructure does not allow to run sidecar containers to perform
