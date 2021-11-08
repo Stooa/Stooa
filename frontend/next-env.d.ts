e482b2da5e73a5dfa263724b8e219916fc386ee7
Italo Devoto Ramella <italodr@gmail.com>
2021-11-08 11:10:25 +0100

    Add copyrights to files

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,3 +1,12 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
