2bfa3e4ff69e0ae54c7a63ef9ca743ccf86465e9
Italo Devoto Ramella <italodr@gmail.com>
2021-11-08 11:17:30 +0100

    Add rule to editorconfig, Fix quote types in new i18n javascript file

--- frontend/i18n.js
+++ frontend/i18n.js
@@ -10,27 +10,27 @@
 const DEFAULT_LOCALE = 'en';
 
 module.exports = {
-  "locales": ["en", "ca", "es"],
-  "defaultLocale": DEFAULT_LOCALE,
-  "pages": {
-    "*": ["common"],
-    "/": ["home"],
-    "/_error": ["_error", "home"],
-    "/404": ["_error", "home"],
-    "/cookies-policy": ["legals"],
-    "/privacy-policy": ["legals"],
-    "/change-password": ["change-password", "password", "form"],
-    "/reset-password/[token]": ["form", "password"],
-    "/edit-profile": ["form", "edit-profile", "register", "password"],
-    "/log-in": ["form", "login"],
-    "/recover-password": ["form", "recover"],
-    "/register": ["form", "register"],
-    "/fb/[fid]": ["fishbowl", "app", "form", "on-boarding"],
-    "/fishbowl/create": ["fishbowl", "form"],
-    "/fishbowl/detail/[fid]": ["fishbowl", "form"],
-    "/fishbowl/thankyou/[fid]": ["fishbowl", "form", "home"]
+  locales: ['en', 'ca', 'es'],
+  defaultLocale: DEFAULT_LOCALE,
+  pages: {
+    '*': ['common'],
+    '/': ['home'],
+    '/_error': ['_error', 'home'],
+    '/404': ['_error', 'home'],
+    '/cookies-policy': ['legals'],
+    '/privacy-policy': ['legals'],
+    '/change-password': ['change-password', 'password', 'form'],
+    '/reset-password/[token]': ['form', 'password'],
+    '/edit-profile': ['form', 'edit-profile', 'register', 'password'],
+    '/log-in': ['form', 'login'],
+    '/recover-password': ['form', 'recover'],
+    '/register': ['form', 'register'],
+    '/fb/[fid]': ['fishbowl', 'app', 'form', 'on-boarding'],
+    '/fishbowl/create': ['fishbowl', 'form'],
+    '/fishbowl/detail/[fid]': ['fishbowl', 'form'],
+    '/fishbowl/thankyou/[fid]': ['fishbowl', 'form', 'home']
   },
-  "loadLocaleFrom": async (lang, ns) => {
+  loadLocaleFrom: async (lang, ns) => {
     const locales = await import(`./locales/${lang}/${ns}.json`).then((m) => m.default);
     const defaultLocales = await import(`./locales/${DEFAULT_LOCALE}/${ns}.json`).then((m) => m.default);
     return { ...defaultLocales, ...locales };
