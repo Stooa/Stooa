3609e3836c3fef2f739b6b9f3c1e7491645a0664
Italo Devoto Ramella <italodr@gmail.com>
2021-11-08 11:28:01 +0100

    Update copyright text

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,15 +1,8 @@
 /*!
- * This file is part of the Stooa.
+ * This file is part of the Stooa codebase.
  *
- * (c) Stooa Team 2021
+ * (c) 2020 - present Runroom SL
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
