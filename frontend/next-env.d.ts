e6da22cbeae7f31204934e81b65d2b1a8c041722
Italo Devoto Ramella <italodr@gmail.com>
2021-11-08 11:21:09 +0100

    Update copyright

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,7 +1,7 @@
 /*!
- * This file is part of the Stooa codebase.
+ * This file is part of the Stooa.
  *
- * (c) 2020 - present Runroom SL
+ * (c) Stooa Team 2021
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
