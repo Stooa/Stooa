9304ed9582707df935198bf7c76836d7381e56c6
Italo Devoto Ramella <italodr@gmail.com>
2021-11-08 17:40:49 +0100

    Fallback to default language (#71)
    * Add copyrights to files
    * Add rule to editorconfig
    * Fix quote types in new i18n javascript file

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,7 +1,7 @@
 /*!
- * This file is part of the Stooa.
+ * This file is part of the Stooa codebase.
  *
- * (c) Stooa Team 2021
+ * (c) 2020 - present Runroom SL
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
