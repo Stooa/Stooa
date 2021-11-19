9694da362322cc95419ce6a4ce3b95e976d0a8a5
Aitor Errasti <azarparalelo@gmail.com>
2021-11-19 09:08:26 +0100

    wip

--- frontend/lib/LocaleCookie.ts
+++ frontend/lib/LocaleCookie.ts
@@ -13,7 +13,7 @@ const LocaleCookie = () => {
   const COOKIE_LOCALE = 'NEXT_LOCALE';
   const COOKIE_OPTIONS = { path: '/', domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN };
   const COOKIE_REFRESH_DAYS = 100;
-  
+
   const setLocaleCookie = (locale: string) => {
     cookie.set(COOKIE_LOCALE, locale, { ...COOKIE_OPTIONS, expires: COOKIE_REFRESH_DAYS });
   };
