94c6e8de47e5669056971bd6cd2bef459fe62ef9
Aitor Errasti <azarparalelo@gmail.com>
2021-11-26 12:58:04 +0100

    Add french language in backend (#102)
    * Add french language in backend
    
    * Change login french message

--- backend/tests/Functional/LoginFunctionalTest.php
+++ backend/tests/Functional/LoginFunctionalTest.php
@@ -102,5 +102,6 @@ class LoginFunctionalTest extends ApiTestCase
         yield ['es', 'Credenciales no válidas'];
         yield ['en', 'Invalid credentials'];
         yield ['ca', 'Credencials no vàlides'];
+        yield ['fr', 'Informations d\'identification invalides.'];
     }
 }
