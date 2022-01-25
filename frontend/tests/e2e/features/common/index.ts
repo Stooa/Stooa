126b38242ae3efbdccec154ceef2348885bf0b49
Jordi Sala Morales <jordism91@gmail.com>
2022-01-25 17:26:12 +0100

    Fix tests E2E

--- frontend/tests/e2e/features/common/index.ts
+++ frontend/tests/e2e/features/common/index.ts
@@ -1,17 +1,17 @@
 import { Then, When } from "cypress-cucumber-preprocessor/steps";
 
 When('navigates to {string}', (url = '') => {
-  cy.visit(url)
+  cy.visit(url, { timeout: 10000 })
 });
 
 When('clicks on {string}', (text = '') => {
-  cy.findAllByRole('link', { name: text }).first().click()
+  cy.findAllByRole('link', { name: text }).first().click({ force: true })
 });
 
 Then('sees {string}', (text = '') => {
-  cy.findByText(text);
+  cy.findByText(text).should('be.visible');
 });
 
 Then('gets redirect to {string}', (url = '') => {
-  cy.location('pathname').should('eq', url);
+  cy.location('pathname', { timeout: 10000 }).should('eq', url);
 });
