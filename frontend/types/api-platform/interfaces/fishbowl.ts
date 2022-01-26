b18d0bead322dc9334c260e74717e56ca37f7587
aitor <aitor@runroom.com>
2022-01-26 13:31:06 +0100

    Fishbowl type updated

--- frontend/types/api-platform/interfaces/fishbowl.ts
+++ frontend/types/api-platform/interfaces/fishbowl.ts
@@ -1,20 +1,12 @@
-/*!
- * This file is part of the Stooa codebase.
- *
- * (c) 2020 - present Runroom SL
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 export interface Fishbowl {
   "@id"?: string;
-  name: string;
+  name?: string;
   description?: string;
   startDateTime: Date;
   timezone: string;
   locale: string;
   duration: Date;
+  hasIntroduction?: boolean;
   readonly slug?: string;
   readonly host?: string;
   readonly currentStatus?: string;
