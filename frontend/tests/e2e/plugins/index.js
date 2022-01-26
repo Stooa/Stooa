aca9a86362a3739c0c4204ad6e8e1419fbaa7df0
Jordi Sala Morales <jordism91@gmail.com>
2022-01-26 09:25:06 +0100

    Fix cypress

--- frontend/tests/e2e/plugins/index.js
+++ frontend/tests/e2e/plugins/index.js
@@ -9,8 +9,9 @@
 
 import { defaultOptions } from '@cypress/browserify-preprocessor';
 import coverage from '@cypress/code-coverage/task';
-import cucumber from 'cypress-cucumber-preprocessor';
 import { sync } from 'resolve';
+
+const cucumber = require('cypress-cucumber-preprocessor').default // eslint-disable-line @typescript-eslint/no-var-requires
 
 export default function handler(on, config) {
   coverage(on, config);
