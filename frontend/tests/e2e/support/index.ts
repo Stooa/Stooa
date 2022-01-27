cc84b378b692f476dd5529128662fe317b02d221
Jordi Sala Morales <jordism91@gmail.com>
2022-01-27 18:11:23 +0100

    Add more tests for the home

--- frontend/tests/e2e/support/index.ts
+++ frontend/tests/e2e/support/index.ts
@@ -9,3 +9,12 @@
 
 import '@testing-library/cypress/add-commands';
 import '@cypress/code-coverage/support';
+import { userAuthToken } from './tokens';
+
+Cypress.Commands.add('login', (authToken = userAuthToken) => {
+  cy.setCookie('token', authToken);
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.clearCookie('token');
+});
