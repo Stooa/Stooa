ebb19a657b5a3cbd4f1ca61847aac1f711bad78c
Jordi Sala Morales <jordism91@gmail.com>
2022-01-27 18:48:10 +0100

    Fix psalm

--- backend/src/EventSubscriber/JWTCreatedSubscriber.php
+++ backend/src/EventSubscriber/JWTCreatedSubscriber.php
@@ -87,9 +87,9 @@ class JWTCreatedSubscriber implements EventSubscriberInterface
         $currentRequest = $this->requestStack->getCurrentRequest();
 
         if (null !== $currentRequest) {
-            $slug = $currentRequest->get('room');
+            $slug = $currentRequest->query->get('room');
 
-            if (null !== $slug && $this->fishbowlService->canFishbowlStart($slug, $user)) {
+            if (null !== $slug && \is_string($slug) && $this->fishbowlService->canFishbowlStart($slug, $user)) {
                 return $slug;
             }
         }
