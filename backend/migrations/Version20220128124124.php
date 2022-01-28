d70a83c2c17adea663beac756d65b8f5ec3ef568
aitor <aitor@runroom.com>
2022-01-28 13:48:48 +0100

    Add migration

---
+++ backend/migrations/Version20220128124124.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220128124124 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE fishbowl ADD is_fishbowl_now TINYINT(1) NOT NULL');
+
+        // All the previous fishbowls has the values to false
+        $this->addSql('UPDATE fishbowl SET is_fishbowl_now = 0');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE fishbowl DROP is_fishbowl_now');
+    }
+}
