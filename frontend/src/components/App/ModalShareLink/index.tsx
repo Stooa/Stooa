dff8b46c69c21991ff9bdd150f4202b715ee6de5
aitor <aitor@runroom.com>
2022-01-31 12:18:10 +0100

    Create modal share link component

---
+++ frontend/src/components/App/ModalShareLink/index.tsx
@@ -0,0 +1,44 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {React, useState} from 'react';
+import useTranslation from 'next-translate/useTranslation';
+
+import Modal from '@/ui/Modal';
+import Cross from '@/ui/svg/cross.svg';
+import CopyUrl from '@/components/Common/CopyUrl';
+import {useStooa} from "@/contexts/StooaManager";
+
+const ModalShareLink: React.FC = () => {
+  const { t } = useTranslation('fishbowl');
+  const { data } = useStooa();
+  const [show, setShow] = useState(true);
+
+  const closeModal = () => {
+    setShow(false);
+  }
+  
+  return (
+    data.isFishbowlNow && show &&
+    <Modal>
+      <div className="content">
+        <button className="close" onClick={closeModal}>
+          <Cross />
+        </button>
+        <h2 className="app-lg">{t('shareModal.title')}</h2>
+        <p className="description">{t('shareModal.description')}</p>
+        <div className="modal-footer">
+          <CopyUrl className="centered" data={data} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ModalShareLink;
--- frontend/src/lib/gql/Fishbowl.js
+++ frontend/src/lib/gql/Fishbowl.js
@@ -63,6 +63,7 @@ const GET_FISHBOWL = gql`
       endDateTimeTz
       durationFormatted
       slug
+      isFishbowlNow
     }
   }
 `;
