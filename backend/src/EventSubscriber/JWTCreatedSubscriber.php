22fb478afd45f0fec73f9c999be1038002ea02ff
Aitor Errasti <azarparalelo@gmail.com>
2022-02-02 12:13:13 +0100

    [Fix] Token room value is not refreshed correctly (#281)
    * Fix token room value is not refreshed correctly
    
    * Fix psalm error
    
    * Fix test
    
    * remove
    
    * Fix functional tets
    
    * Remove unused room parameter

--- backend/src/EventSubscriber/JWTCreatedSubscriber.php
+++ backend/src/EventSubscriber/JWTCreatedSubscriber.php
@@ -87,7 +87,7 @@ class JWTCreatedSubscriber implements EventSubscriberInterface
         $currentRequest = $this->requestStack->getCurrentRequest();
 
         if (null !== $currentRequest) {
-            $slug = $currentRequest->query->get('room');
+            $slug = $currentRequest->request->get('room');
 
             if (null !== $slug && \is_string($slug) && $this->fishbowlService->canFishbowlStart($slug, $user)) {
                 return $slug;
