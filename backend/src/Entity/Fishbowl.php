a03d5a38f5578714d24f1487e47da2bab59bc797
aitor <aitor@runroom.com>
2022-02-07 13:08:51 +0100

    Add get fishbowls endpoint

--- backend/src/Entity/Fishbowl.php
+++ backend/src/Entity/Fishbowl.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Action\NotFoundAction;
 use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\FishbowlRepository;
@@ -41,11 +40,7 @@ use Webmozart\Assert\Assert as MAssert;
  *     normalizationContext={"groups"={"fishbowl:read"}},
  *     denormalizationContext={"groups"={"fishbowl:write"}},
  *     collectionOperations={
- *         "get"={
- *             "controller"=NotFoundAction::class,
- *             "read"=false,
- *             "output"=false,
- *         },
+ *         "get",
  *         "post"={"security"="is_granted('ROLE_USER')"}
  *     },
  *     itemOperations={
