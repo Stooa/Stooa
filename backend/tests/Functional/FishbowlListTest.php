fff37380cfd8d760ebd6c7e391e6a4beb9b21a66
aitor <aitor@runroom.com>
2022-02-07 13:24:49 +0100

    Add tests

---
+++ backend/tests/Functional/FishbowlListTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Fishbowl;
+use App\Entity\User;
+use App\Factory\FishbowlFactory;
+use App\Factory\UserFactory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class FishbowlListTest extends ApiTestCase
+{
+    use Factories;
+    use ResetDatabase;
+
+    private const ADMIN_PASSWORD = '$argon2id$v=19$m=65536,t=4,p=1$37ytdOiVjLdUPFfPRDALmA$xZsJ/uHJ1nTklxYMq1WrjhEPPN2E1HOtVXAyf4rTTV0';
+    private User $host;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->host = UserFactory::createOne([
+            'email' => 'host@stooa.com',
+            'password' => self::ADMIN_PASSWORD,
+            'active' => true,
+        ])->object();
+
+        FishbowlFactory::createOne([
+           'host' => $this->host
+        ]);
+
+        FishbowlFactory::createOne();
+    }
+
+    /** @test */
+    public function itGetsHostFishbowlsCorrectly(): void
+    {
+        $hostToken = $this->logIn($this->host);
+
+        static::createClient()->request('GET', '/fishbowls', [
+            'auth_bearer' => $hostToken,
+        ]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        $this->assertJsonContains([
+            '@context' => '/contexts/Fishbowl',
+            '@id' => '/fishbowls',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 1,
+        ]);
+
+        $this->assertMatchesResourceCollectionJsonSchema(Fishbowl::class);
+    }
+
+    private function logIn(User $user): string
+    {
+        $jwtManager = static::$container->get('lexik_jwt_authentication.jwt_manager');
+
+        return $jwtManager->create($user);
+    }
+}
