bb4292353af80929b012a862df99bef71ab07c72
aitor <aitor@runroom.com>
2022-02-08 16:20:59 +0100

    Wip

--- frontend/src/components/App/FishbowlList/index.tsx
+++ frontend/src/components/App/FishbowlList/index.tsx
@@ -128,7 +128,7 @@ const FishbowlList = () => {
     console.log(fid);
   };
 
-  const params = new URLSearchParams([['estimatedDateToFinish[before]', tomorrow.toISOString()]]);
+  const params = new URLSearchParams([['estimatedDateToFinish[after]', new Date().toISOString()]]);
 
   const getFishbowls = async () => {
     const auth = await getAuthToken();
