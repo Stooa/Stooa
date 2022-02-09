3705aaba3d8425e5c912263ccfc20067eb63e481
aitor <aitor@runroom.com>
2022-02-09 16:40:27 +0100

    Add tests

--- backend/tests/Functional/FishbowlListFunctionalTest.php
+++ backend/tests/Functional/FishbowlListFunctionalTest.php
@@ -173,6 +173,52 @@ class FishbowlListFunctionalTest extends ApiTestCase
         $this->assertSame($fishbowl->getName(), $response->toArray()['hydra:member'][0]['name']);
     }
 
+    /** @test */
+    public function itGetsFishbowlsOrderedCorrectly(): void
+    {
+        $third = FishbowlFactory::createOne([
+            'name' => 'first',
+            'startDateTime' => new \DateTime('tomorrow'),
+            'duration' => \DateTime::createFromFormat('!H:i', '30:00'),
+            'currentStatus' => Fishbowl::STATUS_NOT_STARTED,
+            'host' => $this->host,
+        ])->object();
+
+        $second = FishbowlFactory::createOne([
+            'name' => 'second',
+            'startDateTime' => new \DateTime('now'),
+            'duration' => \DateTime::createFromFormat('!H:i', '30:00'),
+            'currentStatus' => Fishbowl::STATUS_NOT_STARTED,
+            'host' => $this->host,
+        ])->object();
+
+        $first = FishbowlFactory::createOne([
+            'name' => 'third',
+            'startDateTime' => new \DateTime('yesterday'),
+            'duration' => \DateTime::createFromFormat('!H:i', '30:00'),
+            'currentStatus' => Fishbowl::STATUS_NOT_STARTED,
+            'host' => $this->host,
+        ])->object();
+
+        $hostToken = $this->logIn($this->host);
+
+        $response = static::createClient()->request('GET', '/fishbowls', [
+            'auth_bearer' => $hostToken,
+        ]);
+
+        $this->assertJsonContains([
+            '@context' => '/contexts/Fishbowl',
+            '@id' => '/fishbowls',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 3,
+        ]);
+
+        $this->assertSame($first->getName(), $response->toArray()['hydra:member'][0]['name']);
+        $this->assertSame($second->getName(), $response->toArray()['hydra:member'][1]['name']);
+        $this->assertSame($third->getName(), $response->toArray()['hydra:member'][2]['name']);
+
+    }
+
     private function logIn(User $user): string
     {
         $jwtManager = static::$container->get('lexik_jwt_authentication.jwt_manager');
