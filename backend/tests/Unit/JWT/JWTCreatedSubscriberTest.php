5cef6f7eb45f1730f3652656ac88dfc30d871e0e
Aitor Errasti <azarparalelo@gmail.com>
2022-04-06 10:06:56 +0200

    fix jwt tests namespaces

--- backend/tests/Unit/JWT/JWTCreatedSubscriberTest.php
+++ backend/tests/Unit/JWT/JWTCreatedSubscriberTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace App\Tests\Unit;
+namespace App\Tests\Unit\JWT;
 
 use App\Entity\User;
 use App\EventSubscriber\JWTCreatedSubscriber;
