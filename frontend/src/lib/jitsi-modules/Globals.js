898f3ef9ebebc1c0b760239e5cd583d32a4b1b74
Aitor Errasti <azarparalelo@gmail.com>
2022-04-12 12:17:11 +0200

    auth domain and anonymous domain added

--- frontend/src/lib/jitsi-modules/Globals.js
+++ frontend/src/lib/jitsi-modules/Globals.js
@@ -10,6 +10,8 @@
 const connectionOptions = roomName => ({
   hosts: {
     domain: process.env.NEXT_PUBLIC_XMPP_DOMAIN,
+    anonymousdomain: process.env.NEXT_PUBLIC_XMPP_GUEST_DOMAIN,
+    authdomain: process.env.NEXT_PUBLIC_XMPP_AUTH_DOMAIN,
     muc: process.env.NEXT_PUBLIC_XMPP_MUC_DOMAIN
   },
   serviceUrl: `wss://${process.env.NEXT_PUBLIC_JITSI_HOST}/xmpp-websocket?room=${process.env.NEXT_PUBLIC_JITSI_ROOM_PREFIX}${roomName}`,
