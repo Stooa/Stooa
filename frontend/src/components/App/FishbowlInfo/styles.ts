05b6074c851b2167bb897a97087a3dbc07e077a6
Jose <jesauraoller@gmail.com>
2022-04-25 17:10:39 +0200

    button size

--- frontend/src/components/App/FishbowlInfo/styles.ts
+++ frontend/src/components/App/FishbowlInfo/styles.ts
@@ -42,10 +42,10 @@ const Icon = styled.button`
     border: 1px solid transparent;
     border-radius: 50%;
     display: flex;
-    height: ${space(2)};
+    height: ${space(3.25)};
     justify-content: center;
     transition: background-color 0.1s ease-out;
-    width: ${space(2)};
+    width: ${space(3.25)};
     will-change: background-color;
   }
 
