64406c808ebb5d456f28972572cdefeb923eab0b
Aitor Errasti <azarparalelo@gmail.com>
2022-05-23 09:42:35 +0200

    run prettify

--- frontend/tests/unit/components/app/Seats.test.js
+++ frontend/tests/unit/components/app/Seats.test.js
@@ -13,9 +13,9 @@ import { IConferenceStatus } from '@/jitsi/Status';
 import I18nProvider from 'next-translate/I18nProvider';
 
 import appEN from 'locales/en/app.json';
-import {StooaProvider} from "@/contexts/StooaManager";
+import { StooaProvider } from '@/contexts/StooaManager';
 import { MockedProvider } from '@apollo/client/testing';
-import { StateProvider } from "@/contexts/AppContext";
+import { StateProvider } from '@/contexts/AppContext';
 
 const introState = {
   fishbowlReady: true,
@@ -36,24 +36,22 @@ jest.mock('next/router', () => ({
   }
 }));
 
-jest.mock("@/components/App/HostContextActions", () => ({children}) => <>{children}</>);
+jest.mock('@/components/App/HostContextActions', () => ({ children }) => <>{children}</>);
 
 const renderWithContext = state => {
   render(
     <I18nProvider lang={'en'} namespaces={{ app: appEN }}>
       <StateProvider value={[state]}>
-      <MockedProvider>
+        <MockedProvider>
           <Seats />
-      </MockedProvider>
+        </MockedProvider>
       </StateProvider>
     </I18nProvider>
   );
 };
 
 describe('Unit test of fishbowl seats', () => {
-
   it('Unstarted fishbowl with all unavailable seats', () => {
-
     renderWithContext(introState);
 
     const seats = screen.getAllByText('Seat unavailable');
