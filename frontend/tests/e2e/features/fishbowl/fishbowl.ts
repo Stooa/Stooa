4b399073cde647ba9ac45fa96385992b9e382a6f
Aitor Errasti <azarparalelo@gmail.com>
2022-06-16 13:57:25 +0200

    Fishbowl e2e test (#480)
    * e2e tests
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * remove unused json file
    
    * Fix tests

---
+++ frontend/tests/e2e/features/fishbowl/fishbowl.ts
@@ -0,0 +1,160 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { hasOperationName } from '../../utils/graphql-test-utils';
+import { makeGQLCurrentFishbowl, makeGQLTomorrowFishbowl } from '../../factories/fishbowl';
+
+When('navigates to future fishbowl', () => {
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-status/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      status: 'NOT_STARTED'
+    }
+  });
+
+  cy.visit('/en/fb/test-fishbowl', { timeout: 10000 });
+});
+
+When('navigates to fishbowl', () => {
+  const bySlugQueryFishbowl = makeGQLCurrentFishbowl();
+
+  cy.setCookie('share_link', bySlugQueryFishbowl.slug);
+  cy.setCookie('on_boarding_moderator', 'true');
+
+  cy.intercept('POST', 'https://localhost:8443/graphql', req => {
+    if (hasOperationName(req, 'BySlugQueryFishbowl')) {
+      req.reply({
+        data: {
+          bySlugQueryFishbowl
+        }
+      });
+    }
+  }).as('gqlFishbowlBySlugQuery');
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-status/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      status: 'NOT_STARTED'
+    }
+  });
+
+  cy.visit('/en/fb/test-fishbowl', { timeout: 10000 });
+});
+
+Then('sees tomorrow fishbowl information page', () => {
+  const bySlugQueryFishbowl = makeGQLTomorrowFishbowl();
+
+  cy.intercept('POST', 'https://localhost:8443/graphql', req => {
+    if (hasOperationName(req, 'BySlugQueryFishbowl')) {
+      req.reply({
+        data: {
+          bySlugQueryFishbowl
+        }
+      });
+    }
+  }).as('gqlFishbowlBySlugQuery');
+
+  cy.get('[data-testid=fishbowl-name]').should('exist');
+  cy.get('[data-testid=fishbowl-name]').should('have.text', bySlugQueryFishbowl.name);
+
+  cy.get('[data-testid=fishbowl-description]').should('exist');
+  cy.get('[data-testid=fishbowl-description]').should('have.text', bySlugQueryFishbowl.description);
+
+  cy.screenshot();
+});
+
+When('can access to pre fishbowl', () => {
+  cy.intercept('POST', 'https://localhost:8443/graphql', req => {
+    if (hasOperationName(req, 'IsCreatorOfFishbowl')) {
+      req.reply({
+        data: {
+          isCreatorOfFishbowl: {
+            currentStatus: 'test-me-fishbowl'
+          }
+        }
+      });
+    }
+  }).as('gqlIsCreatorOfFishbowl');
+
+  cy.get('[data-testid=pre-join-title]').should('exist');
+  cy.get('[data-testid=pre-join-cancel]').should('exist');
+
+  cy.screenshot();
+});
+
+When('sees the fishbowl page', () => {
+  const bySlugQueryFishbowl = makeGQLCurrentFishbowl();
+
+  cy.intercept('POST', 'https://localhost:8443/graphql', req => {
+    if (hasOperationName(req, 'BySlugQueryFishbowl')) {
+      req.reply({
+        data: {
+          bySlugQueryFishbowl
+        }
+      });
+    }
+  }).as('gqlFishbowlBySlugQuery');
+
+  cy.wait('@gqlFishbowlBySlugQuery');
+
+  cy.get('[data-testid=seat]').should('have.length', 5);
+
+  cy.screenshot();
+});
+
+When('starts fishbowl', () => {
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-status/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      status: 'NOT_STARTED'
+    }
+  });
+
+  cy.intercept('GET', 'https://localhost:8443/en/ping/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      response: true
+    }
+  });
+
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-participants/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      response: []
+    }
+  });
+
+  cy.contains('Start the fishbowl').click();
+
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-status/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      status: 'RUNNING'
+    }
+  });
+
+  cy.get('[data-testid=finish-fishbowl]').should('exist');
+
+  cy.screenshot();
+});
+
+Then('finishes a fishbowl', () => {
+  cy.intercept('GET', 'https://localhost:8443/en/fishbowl-status/test-fishbowl', {
+    statusCode: 200,
+    body: {
+      status: 'FINISHED'
+    }
+  });
+
+  cy.contains('End fishbowl').click();
+
+  cy.wait(3500);
+
+  cy.get('[data-testid=finished-fishbowl]').should('exist');
+});
