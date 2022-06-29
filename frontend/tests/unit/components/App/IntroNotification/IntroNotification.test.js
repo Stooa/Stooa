bf6d20ee9bff45496c9be29ff2c6dc05553efc67
eddsaura <jesauraoller@gmail.com>
2022-06-29 16:07:49 +0200

    Feature: Add tooltip to reaction button (#497)
    * Add tooltip
    
    * Fix x coordinate
    
    * Delete delay from hide
    
    * Rounded and no triangle
    
    * Translations and width fix
    
    * Update test and svg name
    
    * New component and logic in the IntroNotification
    
    * Add Intro Notification unit test
    
    * Add Footer Test
    
    * Fix footer test
    
    * Change toolbar mock
    
    * Mock individualy
    
    * typo
    
    * Remove console
    
    * Gave up
    
    * Delete comments

---
+++ frontend/tests/unit/components/App/IntroNotification/IntroNotification.test.js
@@ -0,0 +1,26 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import IntroNotification from '@/components/App/IntroNotification';
+import { fireEvent, render } from '@testing-library/react';
+
+describe('Intro Notification component', () => {
+  it('It should render and close when clicking cross button', async () => {
+    const { getByTestId, getByRole } = render(<IntroNotification />);
+
+    const introNotificationElement = getByTestId('intro-notification');
+
+    expect(introNotificationElement).toBeInTheDocument();
+
+    const closeButton = getByRole('button');
+    fireEvent.click(closeButton);
+
+    expect(introNotificationElement).not.toBeInTheDocument();
+  });
+});
