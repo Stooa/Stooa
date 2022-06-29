d47fa8314e19d1192ecf5fffc88abc50ac8d8103
eddsaura <jesauraoller@gmail.com>
2022-06-29 16:07:49 +0200

    Feature: Add tooltip to reaction button (#497)
    * Add tooltip
    
    * Fix x coordinate
    
    * Delete delay from hide
    
    * Rounded and no triangle
    
    * Translations and width fix
    
    * Update test and svg name
    
    * New component and logic in the IntroNotification
    
    * Add Intro Notification unit test
    
    * Add Footer Test
    
    * Fix footer test
    
    * Change toolbar mock
    
    * Mock individualy
    
    * typo
    
    * Remove console
    
    * Gave up
    
    * Delete comments

--- frontend/src/components/App/Reactions/ReactionEmoji/styles.ts
+++ frontend/src/components/App/Reactions/ReactionEmoji/styles.ts
@@ -7,13 +7,42 @@
  * file that was distributed with this source code.
  */
 
-import { media } from '@/ui/helpers';
+import { media, space } from '@/ui/helpers';
+import { COLOR_NEUTRO_100 } from '@/ui/settings';
 import styled from 'styled-components';
+
+const StyledTooltip = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 24px;
+  width: max-content;
+  padding: ${space()} ${space(2)};
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: calc(100% + ${space(1.25)});
+  border-radius: 46px;
+
+  color: ${COLOR_NEUTRO_100};
+  background-color: hsla(0, 59%, 0%, 0.65);
+  backdrop-filter: blur(12px);
+
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0;
+
+  &.show {
+    transition: opacity 0.3s 0.3s ease-in-out;
+    opacity: 1;
+  }
+`;
 
 const StyledEmojiReaction = styled.div`
   --translateY: 0;
   --emojiScale: 0.9;
 
+  position: relative;
   cursor: pointer;
   transition: transform 0.15s cubic-bezier(0.22, 1, 0.36, 1);
 
@@ -43,4 +72,4 @@ const StyledEmojiReaction = styled.div`
   }
 `;
 
-export { StyledEmojiReaction };
+export { StyledEmojiReaction, StyledTooltip };
