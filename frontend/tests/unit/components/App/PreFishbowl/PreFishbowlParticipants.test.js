8fddabb139d7c2ea0134a5073c9b079e4cf33f68
Aitor Errasti <azarparalelo@gmail.com>
2022-07-19 12:40:35 +0200

    add tests

--- frontend/tests/unit/components/App/PreFishbowl/PreFishbowlParticipants.test.js
+++ frontend/tests/unit/components/App/PreFishbowl/PreFishbowlParticipants.test.js
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import PreFishbowlParticipants from '@/components/App/PreFishbowl/PreFishbowlParticipants';
 import { useRouter } from 'next/router';
 import { useStateValue } from '@/contexts/AppContext';
@@ -19,6 +19,9 @@ jest.mock('@/lib/analytics');
 jest.mock('@/contexts/AppContext');
 jest.mock('next/router');
 jest.mock('@/repository/ApiParticipantRepository');
+jest.mock('@/components/App/ParticipantPlaceholder', () => () => (
+  <mock-participant-placeholder data-testid="mock-placeholder" />
+));
 
 beforeEach(() => {
   useRouter.mockReturnValue({ query: { fid: 12345 } });
@@ -39,13 +42,16 @@ describe('Pre Fishbowl Participants component', () => {
 
     useStateValue.mockReturnValue([{ isGuest: true }]);
 
-    const { getByTestId } = render(<PreFishbowlParticipants />);
+    const { getByTestId, getAllByTestId } = render(<PreFishbowlParticipants />);
 
     const preFishbowlParticipants = getByTestId('prefishbowl-participants');
     expect(preFishbowlParticipants).toBeInTheDocument();
 
     const preFishbowlRegister = getByTestId('prefishbowl-register');
     expect(preFishbowlRegister).toBeInTheDocument();
+    
+    const placeholder = getAllByTestId('mock-placeholder');
+    expect(placeholder).toHaveLength(10);
 
     expect(getApiParticipantList).toHaveBeenCalled();
 
@@ -55,10 +61,7 @@ describe('Pre Fishbowl Participants component', () => {
   it('Should render component as host', () => {
     setParticipants([]);
 
-    const { getByTestId, queryByTestId } = render(<PreFishbowlParticipants />);
-
-    const preFishbowlParticipants = getByTestId('prefishbowl-participants');
-    expect(preFishbowlParticipants).toBeInTheDocument();
+    const { queryByTestId } = render(<PreFishbowlParticipants />);
 
     const preFishbowlRegister = queryByTestId('prefishbowl-register');
     expect(preFishbowlRegister).not.toBeInTheDocument();
