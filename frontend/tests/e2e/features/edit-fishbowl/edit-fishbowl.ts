b5089cd780ee0503876888edf2c2f7bdfcbb75eb
Jordi Sala Morales <jordism91@gmail.com>
2022-07-27 12:16:16 +0200

    Stricter Eslint Part 1 (#560)
    * Stricter Eslint
    
    * Apply suggestions from code review

--- frontend/tests/e2e/features/edit-fishbowl/edit-fishbowl.ts
+++ frontend/tests/e2e/features/edit-fishbowl/edit-fishbowl.ts
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { Then, When } from 'cypress-cucumber-preprocessor/steps';
 
 import { modifiedValues } from '../common';
 
