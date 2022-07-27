36a035855a5d6591c9be42261a99173ce0e34e55
Jordi Sala Morales <jordism91@gmail.com>
2022-07-27 12:19:41 +0200

    Apply suggestions from code review

--- frontend/src/contexts/DevicesContext.tsx
+++ frontend/src/contexts/DevicesContext.tsx
@@ -82,7 +82,7 @@ const DevicesProvider = ({ children }) => {
   const selectAudioOutputDevice = (deviceId: string): void => {
     const device = _findDevice(deviceId, devices.audioOutputDevices);
 
-    if (!device) {
+    if (null === device) {
       return;
     }
 
@@ -93,7 +93,7 @@ const DevicesProvider = ({ children }) => {
   const selectAudioInputDevice = (deviceId: string): void => {
     const device = _findDevice(deviceId, devices.audioInputDevices);
 
-    if (!device) {
+    if (null === device) {
       return;
     }
 
@@ -104,7 +104,7 @@ const DevicesProvider = ({ children }) => {
   const selectVideoDevice = (deviceId: string): void => {
     const device = _findDevice(deviceId, devices.videoDevices);
 
-    if (!device) {
+    if (null === device) {
       return;
     }
 
