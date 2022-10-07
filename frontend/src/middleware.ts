90ed235b7b34356668d2e7d9f3c6495c3693f18a
Jose <jesauraoller@gmail.com>
2022-10-07 11:40:02 +0200

    Dynamic code eval

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -8,20 +8,18 @@
  */
 
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import flagsmith from 'flagsmith/next-middleware';
 
-export async function middleware(request: NextRequest) {
+export async function middleware() {
   await flagsmith.init({
     environmentID: 'ser.dPTKAVcscTGXRXjGzXFaNX'
   });
 
   const worldcafe = flagsmith.hasFeature('worldcafe');
 
-  console.log('Saura FLAG', worldcafe);
-
-  //redirect to an account page based on the multivariate flag
-  return NextResponse.redirect(new URL('/', request.url));
+  if (worldcafe) {
+    return NextResponse.redirect('/');
+  }
 }
 
 export const config = {
