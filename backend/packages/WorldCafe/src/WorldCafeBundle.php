4a05ae70e771a62b607005bb291feddf9e05e2f7
Aitor <azarparalelo@gmail.com>
2022-10-19 16:00:28 +0200

    wip

--- backend/packages/WorldCafe/src/WorldCafeBundle.php
+++ backend/packages/WorldCafe/src/WorldCafeBundle.php
@@ -13,8 +13,14 @@ declare(strict_types=1);
 
 namespace App\WorldCafe;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class WorldCafeBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
