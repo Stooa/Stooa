784e82173956b81d52d3fe2e23d195dd800a873e
Aitor Errasti <azarparalelo@gmail.com>
2022-10-25 15:15:38 +0200

    Split backend in bundles (#673)
    * create folders
    
    * wip
    
    * refactor config
    
    * wip
    
    * wip
    
    * wip
    
    * Fix services.yaml
    
    * Fix security
    
    * Create fishbowl bundle
    
    * wip
    
    * wip
    
    * Core Bundle
    
    * move tests
    
    * Fix phpstan config
    
    * Fix psalm configuration
    
    * Fix lint

---
+++ backend/packages/Core/src/Encryption/HalitePasswordEncryption.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Core\Encryption;
+
+use ParagonIE\Halite\KeyFactory;
+use ParagonIE\Halite\Symmetric\Crypto;
+use ParagonIE\HiddenString\HiddenString;
+
+class HalitePasswordEncryption implements PasswordEncryptionInterface
+{
+    public function __construct(
+        private readonly string $key
+    ) {
+    }
+
+    public function encrypt(?string $password): ?string
+    {
+        if (null === $password) {
+            return null;
+        }
+
+        return Crypto::encrypt(
+            new HiddenString($password),
+            KeyFactory::importEncryptionKey(new HiddenString($this->key))
+        );
+    }
+
+    public function decrypt(?string $cipherPassword): ?string
+    {
+        if (null === $cipherPassword) {
+            return null;
+        }
+
+        return Crypto::decrypt(
+            $cipherPassword,
+            KeyFactory::importEncryptionKey(new HiddenString($this->key))
+        )->getString();
+    }
+}
---
+++ backend/packages/Fishbowl/src/Stage/FishbowlValidateStage.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Fishbowl\Stage;
+
+use ApiPlatform\Core\GraphQl\Resolver\Stage\ValidateStageInterface;
+use App\Core\Entity\User;
+use App\Fishbowl\Entity\Fishbowl;
+use App\Fishbowl\Service\FishbowlService;
+use Symfony\Component\Security\Core\Security;
+use Webmozart\Assert\Assert;
+
+class FishbowlValidateStage implements ValidateStageInterface
+{
+    public function __construct(
+        private readonly ValidateStageInterface $decorated,
+        private readonly Security $security,
+        private readonly FishbowlService $service
+    ) {
+    }
+
+    /** @param mixed[] $context */
+    public function __invoke($object, string $resourceClass, string $operationName, array $context): void
+    {
+        if ($object instanceof Fishbowl && null === $object->getId()) {
+            $user = $this->security->getUser();
+
+            if (null !== $user) {
+                Assert::isInstanceOf($user, User::class);
+
+                $object->setHost($user);
+            }
+
+            $object->setSlug($this->service->generateRandomSlug($object));
+
+            $object = $this->service->generateDefaultTitle($object);
+        }
+
+        ($this->decorated)($object, $resourceClass, $operationName, $context);
+    }
+}
