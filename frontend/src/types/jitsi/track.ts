b35b3294e9a6c9b39257311634af8aa6846f08df
Jose <jesauraoller@gmail.com>
2022-11-25 16:33:32 +0100

    Fix track type

--- frontend/src/types/jitsi/track.ts
+++ frontend/src/types/jitsi/track.ts
@@ -1,8 +1,17 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 export type MediaType = 'audio' | 'video' | 'presenter' | 'screenshare';
 
 export interface Track {
   isReceivingData: boolean;
-  jitsiTrack: any;
+  jitsiTrack: Record<string, unknown>;
   lastMediaEvent?: string;
   local: boolean;
   mediaType: MediaType;
