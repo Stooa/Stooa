b302f70bbe2f516dfdb39428168fb93681900125
Aitor Errasti <azarparalelo@gmail.com>
2022-12-15 15:54:31 +0100

    Try to fix e2e tests

--- frontend/tests/e2e/features/screen-share/screen-share.ts
+++ frontend/tests/e2e/features/screen-share/screen-share.ts
@@ -13,7 +13,7 @@ Then('can click on screen share button', () => {
   cy.get('[data-testid=share-screen-button]', { timeout: 10000 }).should('exist');
 
   cy.get('[data-testid=share-screen-button]').click();
-  
+
   // cy.get('[data-testid=share-screen-button]').should('have.class', 'sharing');
   //
   // cy.get('[data-testid=seats-wrapper]').should('have.class', 'sharing');
