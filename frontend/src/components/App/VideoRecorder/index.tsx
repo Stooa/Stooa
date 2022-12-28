be50f214b9cf384188a0264f7382f46fb2f3afa4
Aitor Errasti <azarparalelo@gmail.com>
2022-12-28 17:05:02 +0100

    Fix

--- frontend/src/components/App/VideoRecorder/index.tsx
+++ frontend/src/components/App/VideoRecorder/index.tsx
@@ -1,31 +1,45 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'webrtc-adapter';
 import RecordRTC, {RecordRTCPromisesHandler, invokeSaveAsDialog} from 'recordrtc';
 
 export const VideoRecorder = () => {
-  const foo = async () => {
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    }).then(async function(stream) {
-      let recorder = RecordRTC(stream, {
-        type: 'video'
-      });
-      recorder.startRecording();
+  const [stream, setStream] = useState<MediaStream>(null);
+  const [blob, setBlob] = useState(null);
+  const refVideo = useRef(null);
+  const recorderRef = useRef(null);
 
-      const sleep = m => new Promise(r => setTimeout(r, m));
-      await sleep(3000);
+  const handleRecording = async () => {
 
-      recorder.stopRecording(function() {
-        let blob = recorder.getBlob();
-        invokeSaveAsDialog(blob);
-      });
+    const cameraStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    setStream(cameraStream);
+    recorderRef.current = new RecordRTC(cameraStream, { type: 'video' });
+    recorderRef.current.startRecording();
+  };
+
+  const handleStop = () => {
+    recorderRef.current.stopRecording(() => {
+      setBlob(recorderRef.current.getBlob());
     });
-  }
+  };
+
+  const handleSave = () => {
+    invokeSaveAsDialog(blob);
+  };
+
+  useEffect(() => {
+    if (!refVideo.current) {
+      return;
+    }
+
+    // refVideo.current.srcObject = stream;
+  }, [stream, refVideo]);
+
 
   return (
     <div>
-      <button onClick={foo}>Record</button>
+      <button onClick={handleRecording}>Start   </button>
+      <button onClick={handleStop}>Stop  </button>
+      <button onClick={handleSave}>Save  </button>
     </div>
   );
 };
