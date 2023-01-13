52a143cd94a2f5c5dbd2d5a0ac146f984c505b50
Aitor Errasti <azarparalelo@gmail.com>
2023-01-13 17:01:14 +0100

    Fix phpstan errors

--- backend/packages/Fishbowl/src/State/FishbowlStateProvider.php
+++ backend/packages/Fishbowl/src/State/FishbowlStateProvider.php
@@ -19,14 +19,24 @@ use ApiPlatform\State\ProviderInterface;
 use App\Fishbowl\Entity\Fishbowl;
 use App\Fishbowl\Service\PrivateFishbowlService;
 
+/**
+ * @implements ProviderInterface<Fishbowl>
+ */
 class FishbowlStateProvider implements ProviderInterface
 {
     public function __construct(private readonly PrivateFishbowlService $privateFishbowlService, private readonly CollectionProvider $collectionProvider)
     {
     }
 
+    /**
+     * @param array<string> $uriVariables
+     * @param array<mixed> $context
+     *
+     * @return iterable<Fishbowl>
+     */
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): iterable
     {
+        /** @var iterable<Fishbowl> $fishbowls */
         $fishbowls = $this->collectionProvider->provide($operation, $uriVariables, $context);
 
         foreach ($fishbowls as $fishbowl) {
