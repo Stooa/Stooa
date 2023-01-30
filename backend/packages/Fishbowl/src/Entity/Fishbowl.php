4d9859a7e7c4ff2310215927c841050a66837a62
Aitor Errasti <azarparalelo@gmail.com>
2023-01-30 16:06:14 +0100

    Fix entity spaces

--- backend/packages/Fishbowl/src/Entity/Fishbowl.php
+++ backend/packages/Fishbowl/src/Entity/Fishbowl.php
@@ -130,35 +130,47 @@ class Fishbowl implements \Stringable
      *
      * @phpstan-var array<string, Fishbowl::STATUS_*> $statusChoices
      */
-    public static array $statusChoices = ['Not Started' => self::STATUS_NOT_STARTED, 'Introduction' => self::STATUS_INTRODUCTION, 'Running' => self::STATUS_RUNNING, 'Finished' => self::STATUS_FINISHED];
+    public static array $statusChoices = [
+        'Not Started' => self::STATUS_NOT_STARTED,
+        'Introduction' => self::STATUS_INTRODUCTION,
+        'Running' => self::STATUS_RUNNING,
+        'Finished' => self::STATUS_FINISHED
+    ];
+
     #[Groups(['fishbowl:read'])]
     #[ORM\Id]
     #[ORM\Column(type: 'uuid', unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
     private ?UuidInterface $id = null;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[Assert\Length(max: 255)]
     #[ORM\Column(type: 'string')]
     private ?string $name = null;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[ORM\Column(type: 'text', nullable: true)]
     private ?string $description = null;
+
     #[Groups(['fishbowl:read'])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 255)]
     #[ORM\Column(type: 'string', unique: true)]
     private ?string $slug = null;
+
     #[Groups(['fishbowl:write', 'fishbowl:read'])]
     #[Assert\NotNull]
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'datetime')]
     private ?\DateTimeInterface $startDateTime = null;
+
     #[Groups(['fishbowl:write', 'fishbowl:read'])]
     #[Assert\NotNull]
     #[Assert\Length(max: 255)]
     #[Assert\Timezone]
     #[ORM\Column(type: 'string')]
+
     private ?string $timezone = null;
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[Assert\NotNull]
@@ -173,41 +185,53 @@ class Fishbowl implements \Stringable
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'time')]
     private ?\DateTimeInterface $duration = null;
+
     #[Groups(['fishbowl:read'])]
     #[Assert\NotNull]
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'fishbowls')]
     private ?User $host = null;
+
     #[Groups(['fishbowl:read'])]
     #[Assert\Length(max: 255)]
     #[Assert\Choice([self::STATUS_NOT_STARTED, self::STATUS_INTRODUCTION, self::STATUS_RUNNING, self::STATUS_FINISHED])]
     #[ORM\Column(type: 'string', options: ['default' => self::STATUS_NOT_STARTED])]
     private string $currentStatus = self::STATUS_NOT_STARTED;
+
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $introducedAt = null;
+
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $runnedAt = null;
+
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $finishedAt = null;
+
     #[Assert\Type('\\DateTimeInterface')]
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $finishDateTime = null;
+
     /** @var Collection<int, Participant> */
     #[ORM\OneToMany(mappedBy: 'fishbowl', targetEntity: Participant::class, cascade: ['all'])]
     private Collection $participants;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[ORM\Column(type: 'boolean')]
     private bool $isFishbowlNow = false;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[ORM\Column(type: 'boolean')]
     private bool $hasIntroduction = false;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[ORM\Column(type: 'boolean')]
     private bool $isPrivate = false;
+
     #[ORM\Column(type: 'text', nullable: true)]
     private ?string $password = null;
+
     #[Groups(['fishbowl:read', 'fishbowl:write'])]
     #[Assert\Length(min: 8, max: 255)]
     #[Assert\NotBlank(groups: ['user:create'])]
