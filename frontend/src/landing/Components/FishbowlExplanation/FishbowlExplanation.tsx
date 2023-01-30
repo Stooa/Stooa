32889ea603ecff51f0ec4ed9e77140d1b8c4abbd
Saura <jesauraoller@gmail.com>
2023-01-30 11:33:29 +0100

    Feature/landing iteration (#841)
    * Landing changes
    
    * Adjusting
    
    * Add fixed button
    
    * Fix width
    
    * Spacing
    
    * Image loading
    
    * Reviews
    
    * Update changes
    
    * Change title wip
    
    * Update copies
    
    * Update copies and reviews
    
    * mock observer
    
    * Update tests e2e
    
    * Update mail cta
    
    * Center text
    
    * Update og title
    
    * Fix R3
    
    * missing copy
    
    * Limit related videos to channel
    
    * Add background color maik
    
    * R5 updates
    
    * More updates
    
    * Fix tests
    
    * Change prejoin title
    
    * Change common copy to fishbowl
    
    * Fix final
    
    * Hide mobile nav title footer
    
    * Fix title max width
    
    * Fix image

---
+++ frontend/src/landing/Components/FishbowlExplanation/FishbowlExplanation.tsx
@@ -0,0 +1,67 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Trans from 'next-translate/Trans';
+import useTranslation from 'next-translate/useTranslation';
+import Image from 'next/image';
+import { StyledList } from './styles';
+
+const FishbowlExplanation = () => {
+  const { t } = useTranslation('home');
+  return (
+    <>
+      <StyledList>
+        <li className="animate-item">
+          <div className="with-icon">
+            <Image
+              src="/img/web/explanation/chair.png"
+              alt="Fishbowl chair"
+              width={79}
+              height={72}
+            />
+            <div>
+              <h4 className="title-md">{t('howStooa.firstBulletTitle')}</h4>
+              <p className="body-lg hide-mobile">{t('howStooa.firstBulletSub')}</p>
+            </div>
+          </div>
+          <p className="body-lg hide-desktop">{t('howStooa.firstBulletSub')}</p>
+        </li>
+        <li className="animate-item">
+          <div className="with-icon">
+            <Image
+              src="/img/web/explanation/talking-head.png"
+              alt="Talking head"
+              width={75}
+              height={70}
+            />
+            <div className="animate-item">
+              <h4 className="title-md">{t('howStooa.secondBulletTitle')}</h4>
+              <p className="body-lg hide-mobile">{t('howStooa.secondBulletSub')}</p>
+            </div>
+          </div>
+          <p className="body-lg hide-desktop">{t('howStooa.secondBulletSub')}</p>
+        </li>
+      </StyledList>
+      <p className="body-lg animate-item">
+        <Trans
+          i18nKey="home:howStooa.longExplanation1"
+          components={{ span: <span className="medium" /> }}
+        />
+      </p>
+      <p className="body-lg animate-item">
+        <Trans
+          i18nKey="home:howStooa.longExplanation2"
+          components={{ span: <span className="medium" /> }}
+        />
+      </p>
+    </>
+  );
+};
+
+export default FishbowlExplanation;
