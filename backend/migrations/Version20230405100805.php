287c4aec1138a1e14686c4478e64ff184a56eeec
Aitor Errasti <azarparalelo@gmail.com>
2023-04-05 12:08:20 +0200

    Unify migrations

--- backend/migrations/Version20230405100805.php
+++ backend/migrations/Version20230405100805.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Stooa codebase.
- *
- * (c) 2020 - present Runroom SL
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -19,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230403144853 extends AbstractMigration
+final class Version20230405100805 extends AbstractMigration
 {
     public function getDescription(): string
     {
