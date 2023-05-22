d3f51f0866c635009c5037921b482d6b2b6a15f1
Aitor Errasti <azarparalelo@gmail.com>
2023-05-22 17:14:14 +0200

    Disable transcriptions by default

--- frontend/src/components/App/TranscriptionText/TranscriptionWrapper.tsx
+++ frontend/src/components/App/TranscriptionText/TranscriptionWrapper.tsx
@@ -96,7 +96,6 @@ const TranscriptionWrapper = () => {
   const sentText = useDebounce<object>(textToShow, 3000);
 
   useEffect(() => {
-    console.log(textToShow);
     if (Object.keys(sentText).length > 0) {
       setTextToShow({});
     }
