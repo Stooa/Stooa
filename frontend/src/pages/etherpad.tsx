6c0d35e2e9234f179a105ddd6500f28f5ac56d60
Aitor Errasti <azarparalelo@gmail.com>
2023-05-24 16:53:43 +0200

    add apikey

--- frontend/src/pages/etherpad.tsx
+++ frontend/src/pages/etherpad.tsx
@@ -9,17 +9,16 @@
 import Layout from '@/layouts/Default';
 
 import api from '@/lib/api';
-import Settings from "lib-jitsi-meet/types/hand-crafted/modules/settings/Settings";
-import sessionId = Settings.sessionId;
 import {useState} from "react";
 const Page = () => {
   const [sessionId, setSessionId] = useState('');
+  const apiKey= '0d7b26f659fced90cbdb6b8ab51748b4c86970302a33b9bc371b10215fca9bd6'
   let authorId = null;
   let groupId = null;
 
   // Portal maps the internal userid to an etherpad author.
   api
-    .post('https://localhost:8243/etherpad/api/1/createAuthorIfNotExistsFor?apikey=36760b9f0e63a69ceea4ec5ae44868afd35e1053f8c5e44110125a35ad7f7920&name=Michael&groupMapper=7' )
+    .post(`https://localhost:8243/etherpad/api/1/createAuthorIfNotExistsFor?apikey=${apiKey}&name=Michael&groupMapper=7`)
     .then(res => {
       console.log('Author ID', res.data.data.authorID);
       authorId = res.data.data.authorID;
@@ -30,13 +29,13 @@ const Page = () => {
 
   // Portal maps the internal userid to an etherpad group:
   api
-    .post('https://localhost:8243/etherpad/api/1/createGroupIfNotExistsFor?apikey=36760b9f0e63a69ceea4ec5ae44868afd35e1053f8c5e44110125a35ad7f7920&groupMapper=7' )
+    .post(`https://localhost:8243/etherpad/api/1/createGroupIfNotExistsFor?apikey=${apiKey}&groupMapper=7`)
     .then(res => {
       console.log('Group ID', res.data.data.groupID);
       groupId = res.data.data.groupID;
 
       api
-        .post(`https://localhost:8243/etherpad/api/1/createGroupPad?apikey=36760b9f0e63a69ceea4ec5ae44868afd35e1053f8c5e44110125a35ad7f7920&groupID=${groupId}&padName=test123&text=Hello` )
+        .post(`https://localhost:8243/etherpad/api/1/createGroupPad?apikey=${apiKey}&groupID=${groupId}&padName=test123&text=Hello`)
         .then(res => {
           console.log('Portal creates a pad in the userGroup', res);
         })
@@ -45,7 +44,7 @@ const Page = () => {
         });
       // Portal starts the session for the user on the group:
       api
-        .post(`https://localhost:8243/etherpad/api/1/createSession?apikey=36760b9f0e63a69ceea4ec5ae44868afd35e1053f8c5e44110125a35ad7f7920&groupID=${groupId}&authorID=${authorId}&validUntil=1690195784` )
+        .post(`https://localhost:8243/etherpad/api/1/createSession?apikey=${apiKey}&groupID=${groupId}&authorID=${authorId}&validUntil=1690195784` )
         .then(res => {
           console.log('Session ID', res.data.data.sessionID);
           setSessionId(res.data.data.sessionID);
@@ -63,7 +62,10 @@ const Page = () => {
     return (
       <Layout center={false} title="Etherpad">
         <iframe src={`https://localhost:8243/etherpad/auth_session?sessionID=${sessionId}&padName=test123`} width="600" height="400"></iframe>
-        <iframe src={`https://localhost:8243/etherpad/p/lolololololo`} width="600" height="400"></iframe>
+        <iframe src={`https://localhost:8243/etherpad/p/one`} width="600" height="400"></iframe>
+        <iframe
+          src={`https://localhost:8243/etherpad/p/two?showChat=false&showLineNumbers=false&showControls=false&userName=Tronco`}
+          width="600" height="400"></iframe>
       </Layout>
     );
   } else {
