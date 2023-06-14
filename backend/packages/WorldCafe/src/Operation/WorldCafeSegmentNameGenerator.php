0b94a71a156889afb6afcf9e9f38d2a5eb44cee6
Aitor Errasti <azarparalelo@gmail.com>
2023-06-14 11:32:24 +0200

    fix

--- backend/packages/WorldCafe/src/Operation/WorldCafeSegmentNameGenerator.php
+++ backend/packages/WorldCafe/src/Operation/WorldCafeSegmentNameGenerator.php
@@ -1,20 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\WorldCafe\Operation;
 
+use ApiPlatform\Metadata\Util\Inflector;
 use ApiPlatform\Operation\PathSegmentNameGeneratorInterface;
 
 class WorldCafeSegmentNameGenerator implements PathSegmentNameGeneratorInterface
 {
     public function getSegmentName(string $name, bool $collection = true): string
     {
-        $name = $this->underscoreize($name);
+        if ('WorldCafe' === $name) {
+            return $collection ? 'world_cafes' : 'world_cafe';
+        }
 
-        return $name;
-    }
+        $name = Inflector::tableize($name);
 
-    private function underscoreize(string $string): string
-    {
-        return strtolower(preg_replace('~(?<=\\w)([A-Z])~', '_$1', $string));
+        return $collection ? Inflector::pluralize($name) : $name;
     }
 }
