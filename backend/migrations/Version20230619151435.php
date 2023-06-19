c28fb5bef6e2fd6b16d3dcd883054845c876ff9f
Aitor Errasti <azarparalelo@gmail.com>
2023-06-19 17:16:26 +0200

    Migration updated

--- backend/migrations/Version20230619151435.php
+++ backend/migrations/Version20230619151435.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Stooa codebase.
- *
- * (c) 2020 - present Runroom SL
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -20,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230614143209 extends AbstractMigration
+final class Version20230619151435 extends AbstractMigration
 {
     use ContainerAwareTrait;
 
@@ -37,7 +28,7 @@ final class Version20230614143209 extends AbstractMigration
         );
 
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE question (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', world_cafe_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_B6F7494E8855E250 (world_cafe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', world_cafe_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', position INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_B6F7494E8855E250 (world_cafe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE world_cafe (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', host_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', round_minutes INT DEFAULT 10 NOT NULL, has_extra_round_time TINYINT(1) NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, slug VARCHAR(255) NOT NULL, start_date_time DATETIME NOT NULL, timezone VARCHAR(255) NOT NULL, locale VARCHAR(255) NOT NULL, current_status VARCHAR(255) DEFAULT \'not_started\' NOT NULL, finished_at DATETIME DEFAULT NULL, finish_date_time DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_9179BDC4989D9B62 (slug), INDEX IDX_9179BDC41FB8D185 (host_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE world_cafe_topics (world_cafe_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', topic_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', INDEX IDX_7FD1EF68855E250 (world_cafe_id), INDEX IDX_7FD1EF61F55203D (topic_id), PRIMARY KEY(world_cafe_id, topic_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E8855E250 FOREIGN KEY (world_cafe_id) REFERENCES world_cafe (id)');
