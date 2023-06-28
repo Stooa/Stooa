8a833795c6a99772422321769a9480e306c7fa74
Aitor Errasti <azarparalelo@gmail.com>
2023-06-28 16:50:18 +0200

    Add worldcafe and question interfaces

---
+++ frontend/src/types/api-platform/interfaces/worldcafe.ts
@@ -0,0 +1,25 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Question} from "@/types/api-platform/interfaces/question";
+
+export interface WorldCafe {
+  "@id"?: string;
+  roundMinutes?: number;
+  hasExtraRoundTime?: boolean;
+  questions?: Question[];
+  name?: string;
+  description?: string;
+  slug: string;
+  startDateTime: Date;
+  timezone: string;
+  locale: string;
+  currentStatus?: string;
+  readonly startDateTimeTz?: Date;
+}
