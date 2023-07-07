80b50c24685cf4a7298893627b77b3b6ebd2fa4e
Aitor Errasti <azarparalelo@gmail.com>
2023-07-07 08:48:21 +0200

    migration add skip if world_cafe env

--- backend/migrations/Version20230704100601.php
+++ backend/migrations/Version20230704100601.php
@@ -15,12 +15,16 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230704100601 extends AbstractMigration
+final class Version20230704100601 extends AbstractMigration implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     public function getDescription(): string
     {
         return '';
@@ -28,6 +32,11 @@ final class Version20230704100601 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->skipIf(
+            'false' === $this->container->getParameter('world_cafe'),
+            'Skipping migration because World Cafe is disabled'
+        );
+
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE question (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', world_cafe_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', position INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_B6F7494E8855E250 (world_cafe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE world_cafe (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', host_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', round_minutes INT DEFAULT 10 NOT NULL, current_round INT DEFAULT 1 NOT NULL, has_extra_round_time TINYINT(1) NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, slug VARCHAR(255) NOT NULL, start_date_time DATETIME NOT NULL, timezone VARCHAR(255) NOT NULL, locale VARCHAR(255) NOT NULL, current_status VARCHAR(255) DEFAULT \'not_started\' NOT NULL, finished_at DATETIME DEFAULT NULL, finish_date_time DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_9179BDC4989D9B62 (slug), INDEX IDX_9179BDC41FB8D185 (host_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
