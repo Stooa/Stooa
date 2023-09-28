3e249fbdea81b9f7058cbff70c2e3e803e4be814
Aitor Errasti <azarparalelo@gmail.com>
2023-09-28 09:18:15 +0200

    Remove unnecesary state

--- frontend/src/pages/slack.tsx
+++ frontend/src/pages/slack.tsx
@@ -10,7 +10,6 @@
 import Layout from '@/layouts/Default';
 import { useQuery } from '@apollo/client';
 import { GET_SELF_USER } from '@/graphql/User';
-import { useEffect, useState } from 'react';
 import RedirectLink from '@/components/Web/RedirectLink';
 import { ROUTE_FISHBOWL_CREATE } from '@/app.config';
 import Button from '@/components/Common/Button';
@@ -19,26 +18,19 @@ import useTranslation from 'next-translate/useTranslation';
 const Slack = () => {
   const { data } = useQuery(GET_SELF_USER);
   const slackUrl = `https://slack.com/oauth/v2/authorize?scope=incoming-webhook&amp;user_scope=&redirect_uri=${process.env.NEXT_PUBLIC_SLACK_REDIRECT_URL}&client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}`;
-  const [slackWebHook, setSlackWebHook] = useState<string>();
   const { t } = useTranslation('common');
-
-  useEffect(() => {
-    if (data && data.selfUser.slackWebHook) {
-      setSlackWebHook(data.selfUser.slackWebHook);
-    }
-  }, [data]);
 
   return (
     <Layout title="Slack">
       <h1 className="title-md form-title">Slack notifications</h1>
       <a className="body-md bold" href={slackUrl}>
-        {slackWebHook ? 'Update slack connection' : 'Add Slack connection'}
+        {data && data.selfUser.slackWebHook ? 'Update slack connection' : 'Add Slack connection'}
       </a>
       <br></br>
-      {slackWebHook && (
+      {data && data.selfUser.slackWebHook && (
         <>
           <div>
-            <b>Webhook:</b> {slackWebHook}
+            <b>Webhook:</b> {data.selfUser.slackWebHook}
           </div>
           <br></br>
           <RedirectLink href={ROUTE_FISHBOWL_CREATE} passHref>
