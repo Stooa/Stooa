3b7fa7652c1623dc55d8ad93e3fcfc340bf2412e
Aitor Errasti <azarparalelo@gmail.com>
2023-10-16 17:52:30 +0200

    Add hubspot integration

--- frontend/src/components/Web/Avatar/index.tsx
+++ frontend/src/components/Web/Avatar/index.tsx
@@ -15,13 +15,15 @@ import {
   ROUTE_EDIT_PROFILE,
   ROUTE_CHANGE_PASSWORD,
   ROUTE_FISHBOWL_SCHEDULED,
-  ROUTE_FISHBOWL_FINISHED
+  ROUTE_FISHBOWL_FINISHED,
+  ROUTE_HUBSPOT
 } from '@/app.config';
 import { useAuth } from '@/contexts/AuthContext';
 import AvatarIcon from '@/ui/svg/avatar.svg';
 import ChevronDown from '@/ui/svg/chevron-down.svg';
 import Logout from '@/ui/svg/logout.svg';
 import Lock from '@/ui/svg/lock.svg';
+import Url from '@/ui/svg/url.svg';
 import Pencil from '@/ui/svg/pencil.svg';
 import List from '@/ui/svg/list.svg';
 import CheckList from '@/ui/svg/check-list.svg';
@@ -79,6 +81,10 @@ const Avatar: React.FC = () => {
           <Link href={ROUTE_EDIT_PROFILE} className="item">
             <Pencil />
             <span>{t('editProfile')}</span>
+          </Link>
+          <Link href={ROUTE_HUBSPOT} className="item">
+            <Url />
+            <span>Hubspot integration</span>
           </Link>
           <Link href={ROUTE_CHANGE_PASSWORD} className="item">
             <Lock />
