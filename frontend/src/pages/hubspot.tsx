7c3f7ca0803c450cca9f44ce53e7d2c0ffa03ca8
Aitor Errasti <azarparalelo@gmail.com>
2023-10-18 17:16:19 +0200

    show contacts in frontend

--- frontend/src/pages/hubspot.tsx
+++ frontend/src/pages/hubspot.tsx
@@ -10,19 +10,46 @@
 import Layout from '@/layouts/Default';
 import { GET_SELF_USER } from '@/graphql/User';
 import { useQuery } from '@apollo/client';
+import { getHubspotContactList } from '@/repository/HubspotContactRepository';
+import { useEffect, useState } from 'react';
 
 const Hubspot = () => {
   const hubspotUrl = `${process.env.NEXT_PUBLIC_HUBSPOT_URL}?client_id=${process.env.NEXT_PUBLIC_HUBSPOT_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_HUBSPOT_REDIRECT_URL}&scope=crm.objects.contacts.read%20crm.objects.contacts.write`;
   const { data } = useQuery(GET_SELF_USER);
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = () => {
+    getHubspotContactList()
+      .then(contactList => {
+        setContacts(contactList.contacts);
+      })
+      .catch(error => {
+        console.log('[STOOA] Error getting contacts', error);
+      });
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
 
   return (
     <Layout title="Hubspot">
       <h1 className="title-md form-title">Hubspot</h1>
-      <p>
-        <b>
-          {data?.selfUser.hasHubspotRefreshToken ? 'Tiene Refresh token' : 'No tiene Refresh Token'}
-        </b>
-      </p>
+      <b>
+        {data?.selfUser.hasHubspotRefreshToken ? 'Tiene Refresh token' : 'No tiene Refresh Token'}
+      </b>
+      {contacts.map((contact, index) => {
+        return (
+          <div key={index}>
+            <p>
+              <b>name:</b> {contact.properties.firstname}
+            </p>
+            <p>
+              <b>country:</b> {contact.properties.email}
+            </p>
+          </div>
+        );
+      })}
       <br></br>
       <a className="body-md bold" href={hubspotUrl}>
         {data?.selfUser.hasHubspotRefreshToken ? 'Reconectar con Hubspot' : 'Conectar con Hubspot'}
