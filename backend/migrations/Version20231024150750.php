ccdc4ab5acf3ea1cf6d0d7d9708e2494d75a24f4
Aitor Errasti <azarparalelo@gmail.com>
2023-10-24 17:33:37 +0200

    Create migration

---
+++ backend/migrations/Version20231024150750.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231024150750 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD hubspot_refresh_token VARCHAR(255) NOT NULL, ADD last_sync_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP hubspot_refresh_token, DROP last_sync_date');
+    }
+}
