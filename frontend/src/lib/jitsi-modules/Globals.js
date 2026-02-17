8e70aa83a8b9c18573b3b0e0caf2c1427008c942
Aitor Errasti <aitor@runroom.com>
2026-02-17 10:47:48 +0100

    fix globals

--- frontend/src/lib/jitsi-modules/Globals.js
+++ frontend/src/lib/jitsi-modules/Globals.js
@@ -14,7 +14,8 @@ const connectionOptions = roomName => ({
     authdomain: process.env.NEXT_PUBLIC_XMPP_AUTH_DOMAIN,
     muc: process.env.NEXT_PUBLIC_XMPP_MUC_DOMAIN
   },
-  serviceUrl: `wss://${process.env.NEXT_PUBLIC_JITSI_HOST}/xmpp-websocket?room=${process.env.NEXT_PUBLIC_JITSI_ROOM_PREFIX}${roomName}`,
+  serviceUrl: `wss://${process.env.NEXT_PUBLIC_JITSI_HOST}/${process.env.NEXT_PUBLIC_JITSI_ROOM_PREFIX}xmpp-websocket?room=${roomName}`,
+  websocketKeepAliveUrl: `https://${process.env.NEXT_PUBLIC_JITSI_HOST}/${process.env.NEXT_PUBLIC_JITSI_ROOM_PREFIX}_unlock?room=${roomName}`,
   deploymentInfo: {}
 });
 
