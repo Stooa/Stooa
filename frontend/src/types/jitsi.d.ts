15634f3c2555c5ab0b1dad46d9b2ebb96c8af4c6
Aitor Errasti <aitor@runroom.com>
2026-02-17 18:17:17 +0100

    wip

--- frontend/src/types/jitsi.d.ts
+++ frontend/src/types/jitsi.d.ts
@@ -1,1 +1,11 @@
+/*!
+ * This file is part of the Stooa codebase.
+ *
+ * (c) 2020 - present Runroom SL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../../node_modules/lib-jitsi-meet/types/index.d.ts" />
